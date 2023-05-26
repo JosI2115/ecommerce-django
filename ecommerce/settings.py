30aa908bea4653c8964464757cdfb1cb09944979
Josue Ibarra <josue.campos2115@gmail.com>
2023-05-26 17:27:41 -0600

    deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,7 +35,7 @@ DEBUG = 'RENDER' not in os.environ
 
 
 #ALLOWED_HOSTS = ['ecommerce-env.eba-pr8br5qc.us-west-2.elasticbeanstalk.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
