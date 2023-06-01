1b7bfc5c521c4bdeb26ac93e9f447bb1b179efad
Josue Ibarra <josue.campos2115@gmail.com>
2023-05-31 21:13:27 -0600

    media116

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -170,7 +170,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-    STATICFILES_STORAGE = 'django_render.render_storage.RenderStaticStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 RENDER_BACKEND = 'django_render.backends.RenderBackend'
 
