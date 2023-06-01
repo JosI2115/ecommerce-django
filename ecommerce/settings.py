67171fc73fc9a0d6c08ed8beb9bb2aae5a26673c
Josue Ibarra <josue.campos2115@gmail.com>
2023-06-01 10:34:11 -0600

    media32

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -164,7 +164,6 @@ USE_TZ = True
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
