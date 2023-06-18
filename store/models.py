d9b89f683647e5dea193525800e2c11d71645eff
Josue Ibarra <josue.campos2115@gmail.com>
2023-06-18 11:55:41 -0600

    Ia41

--- store/models.py
+++ store/models.py
@@ -70,7 +70,7 @@ class ReviewRating(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100, blank=True)
     review = models.CharField(max_length=500, blank=True)
-    rating = models.FloatField()
+    #rating = models.FloatField()
     ip = models.CharField(max_length=20, blank=True)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
