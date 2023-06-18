1b526448bd28bae9af19e9da5f8780af8100f94f
Josue Ibarra <josue.campos2115@gmail.com>
2023-06-18 11:52:38 -0600

    Ia40

--- store/forms.py
+++ store/forms.py
@@ -4,7 +4,8 @@ from .models import ReviewRating
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = ReviewRating
-        fields = ['subject', 'review', 'rating', 'sentiment_score']
+        fields = ['subject', 'review', 'sentiment_score']
+        #fields = ['subject', 'review', 'rating', 'sentiment_score']
         widgets = {
             'sentiment_score': forms.HiddenInput()
         }
